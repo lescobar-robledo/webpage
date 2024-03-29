76bd6596d9e92e6493f9e6546f76a2b7ff7a61be
Luis Escobar-Robledo <lescobar@Ls-MacBook-Air.local>
2024-03-29 01:01:29 -0400

    Created three new pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://astronaut.github.io',
-    base: 'my-repo',
+//    site: 'https://lescobar-robledo.github.io',
+//    base: '/lecb_tutorial_blog',
 });
