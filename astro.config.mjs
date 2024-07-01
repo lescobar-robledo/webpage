a5ef9e430e3ba37b6031ed5e41d0f34bd5520fa8
Luisa Escobar-Robledo <LAE84@pitt.edu>
2024-07-01 15:16:28 -0400

    test changes to deploy on github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-//    site: 'https://lescobar-robledo.github.io',
-//    base: '/lecb_tutorial_blog',
+    site: 'https://ishihara_lab.github.io',
+    base: '/lecb_tutorial_blog',
 });
