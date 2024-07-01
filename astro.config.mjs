241edf2b1aebeba050186057f14cb25f161db92c
Luisa Escobar-Robledo <LAE84@pitt.edu>
2024-07-01 15:19:18 -0400

    "fixed site link"

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://ishihara_lab.github.io',
+    site: 'https://lescobar-robledo.github.io/ishihara_lab',
     base: '/lecb_tutorial_blog',
 });
