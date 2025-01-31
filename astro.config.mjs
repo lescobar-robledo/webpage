d20a57836958ad94f5bda2858e41267cb67b2b20
Luisa Escobar-Robledo <lescobar@Lus-MacBook-Air.local>
2025-01-31 12:13:44 -0500

    changed base

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,5 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
     site: 'https://lescobar-robledo.github.io/',
-    base: '/lecb_tutorial_blog',
+    base: '/webpage',
 });
